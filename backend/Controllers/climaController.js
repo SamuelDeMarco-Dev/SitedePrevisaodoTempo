eb9ebe6d0fc773b9c02c7cb4e6c6d8ae88bd4b91
Samuel De Marco <samuel.demarco.dev@gmail.com>
2026-07-01 08:56:06 -0300

    fix - Realizado a correção do import no Controller da função getPrevisaoTempo5dias

--- backend/Controllers/climaController.js
+++ backend/Controllers/climaController.js
@@ -1,23 +1,23 @@
 import { getPrevisaoTempo } from '../Services/buscaPrevisao.js';
-import { getPrevisao5dias } from '../Services/buscaPrevisao5dias.js';
+import { getPrevisaoTempo5dias as getPrevisao5diasService } from '../Services/buscaPrevisao5dias.js';
 import { Clima } from '../Models/Clima.js';
 
 export async function getClima(req, res) {
-    const {cidade} = req.query;
-    try{
+    const { cidade } = req.query;
+    try {
         const dados = await getPrevisaoTempo(cidade);
         res.json(new Clima(dados));
-    } catch (erro){
-        res.status(400).json({erro: erro.message});
+    } catch (erro) {
+        res.status(400).json({ erro: erro.message });
     }
 }
 
 export async function getPrevisao5dias(req, res) {
-    const {cidade} = req.query;
-    try{
-        const dados = await getPrevisao5dias(cidade);
+    const { cidade } = req.query;
+    try {
+        const dados = await getPrevisao5diasService(cidade);
         res.json(dados);
-    } catch (erro){
-        res.status(400).json({erro: erro.message});
+    } catch (erro) {
+        res.status(400).json({ erro: erro.message });
     }
 }
